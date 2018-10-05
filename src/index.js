032d7dd4c9145bdfeb0977e871161b1dac08e4e5
Ryan Walters <rwalters@eb.com>
2018-10-05 16:39:52 -0500

    export MediaStrip

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 
 import LazyImage from './components/LazyImage/LazyImage';
 import MediaOverlay from './components/MediaOverlay';
+import MediaStrip from './components/MediaStrip/MediaStrip';
 import Shave from './components/Shave/Shave';
 import Taparoo from './components/Taparoo/Taparoo';
 
 export { LazyImage };
 export { MediaOverlay };
+export { MediaStrip };
 export { Shave };
 export { Taparoo };

35c9e717cdd564ad9eb606dad380a2c9e33dd75b
Ryan Walters <rwalters@eb.com>
2019-03-13 15:36:11 -0500

    update dependencies; add (orphaned) MediaOverlay.lazy.js possibly for future use

---
+++ src/components/MediaOverlay/MediaOverlay.lazy.js
@@ -0,0 +1,31 @@
+import { faSpinnerThird } from '@fortawesome/pro-light-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
+import React, { lazy, Suspense } from 'react';
+import styles from './MediaOverlay.scss';
+
+const LazyMediaOverlay = lazy(() => import('./MediaOverlayContainer'));
+
+// Super pared down version of the overlay, used while loading the real thing
+
+const MediaOverlayLoading = () => (
+  <div className={styles.MediaOverlay}>
+    <div className={styles.background}>
+      <div className={classNames(styles.main, styles.loading)}>
+        <div className="animated fadeIn delay-1s">
+          <FontAwesomeIcon icon={faSpinnerThird} size="2x" spin />
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+// Lazy loaded media overlay
+
+const MediaOverlay = props => (
+  <Suspense fallback={<MediaOverlayLoading />}>
+    <LazyMediaOverlay {...props} />
+  </Suspense>
+);
+
+export default MediaOverlay;

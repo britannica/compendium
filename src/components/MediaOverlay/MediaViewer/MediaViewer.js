aa7a328b3f6fbfe12539d763aeb768ecafd3c6d9
Ryan Walters <rwalters@eb.com>
2019-07-08 13:49:21 -0500

    convert to useContext

--- src/components/MediaOverlay/MediaViewer/MediaViewer.js
+++ src/components/MediaOverlay/MediaViewer/MediaViewer.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleLeft, faAngleRight } from '@fortawesome/pro-light-svg-icons';
 import classNames from 'classnames';
@@ -8,44 +8,44 @@ import MediaLink from '../MediaLink/MediaLink';
 import MediaOverlayContext from '../MediaOverlay.context';
 import styles from './MediaViewer.module.scss';
 
-const MediaViewer = () => (
-  <MediaOverlayContext.Consumer>
-    {({
-      overlayState: { controlsHidden, hasError, localeLabels, mediaIndex, assemblies },
-      handleTap,
-      navigateNextMedia,
-      navigatePreviousMedia,
-    }) => (
-      <Taparoo
-        onTap={handleTap}
-        onSwipeLeft={navigateNextMedia}
-        onSwipeRight={navigatePreviousMedia}
-        className={classNames(styles.MediaViewer, { controlsHidden })}
-      >
-        {hasError ? <Fragment>{localeLabels.ERROR}</Fragment> : <Media />}
-        {assemblies.length > 1 && !controlsHidden && (
-          <Fragment>
-            {mediaIndex > 0 && (
-              <MediaLink
-                assemblyId={assemblies[mediaIndex - 1].assemblyId}
-                className={classNames(styles.mediaArrow, styles.prev, 'd-print-none')}
-              >
-                <FontAwesomeIcon icon={faAngleLeft} size="3x" />
-              </MediaLink>
-            )}
-            {mediaIndex < assemblies.length - 1 && (
-              <MediaLink
-                assemblyId={assemblies[mediaIndex + 1].assemblyId}
-                className={classNames(styles.mediaArrow, styles.next, 'd-print-none')}
-              >
-                <FontAwesomeIcon icon={faAngleRight} size="3x" />
-              </MediaLink>
-            )}
-          </Fragment>
-        )}
-      </Taparoo>
-    )}
-  </MediaOverlayContext.Consumer>
-);
+const MediaViewer = () => {
+  const {
+    overlayState: { controlsHidden, hasError, localeLabels, mediaIndex, assemblies },
+    handleTap,
+    navigateNextMedia,
+    navigatePreviousMedia,
+  } = useContext(MediaOverlayContext);
+
+  return (
+    <Taparoo
+      onTap={handleTap}
+      onSwipeLeft={navigateNextMedia}
+      onSwipeRight={navigatePreviousMedia}
+      className={classNames(styles.MediaViewer, { controlsHidden })}
+    >
+      {hasError ? <Fragment>{localeLabels.ERROR}</Fragment> : <Media />}
+      {assemblies.length > 1 && !controlsHidden && (
+        <Fragment>
+          {mediaIndex > 0 && (
+            <MediaLink
+              assemblyId={assemblies[mediaIndex - 1].assemblyId}
+              className={classNames(styles.mediaArrow, styles.prev, 'd-print-none')}
+            >
+              <FontAwesomeIcon icon={faAngleLeft} size="3x" />
+            </MediaLink>
+          )}
+          {mediaIndex < assemblies.length - 1 && (
+            <MediaLink
+              assemblyId={assemblies[mediaIndex + 1].assemblyId}
+              className={classNames(styles.mediaArrow, styles.next, 'd-print-none')}
+            >
+              <FontAwesomeIcon icon={faAngleRight} size="3x" />
+            </MediaLink>
+          )}
+        </Fragment>
+      )}
+    </Taparoo>
+  );
+};
 
 export default MediaViewer;

aabae5dd3737adf54dcfb0c694a2fe9ad79a01ea
Ryan Walters <ryan@ryanwalters.dev>
2023-03-31 10:51:06 -0500

    fix: remove `title` from `InfogramProp`

--- src/prop-types/InfogramProp.js
+++ src/prop-types/InfogramProp.js
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 
 const InfogramProp = PropTypes.shape({
   filename: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
 });
 
 export default InfogramProp;

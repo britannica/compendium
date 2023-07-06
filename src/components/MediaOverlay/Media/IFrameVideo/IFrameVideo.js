0a206c1e2ed48d6505b32f44501516f828f9d5d7
Ryan Walters <ryan@ryanwalters.dev>
2023-07-06 11:46:09 -0500

    fix(IFrameVideo): remove scrollbars

--- src/components/MediaOverlay/Media/IFrameVideo/IFrameVideo.js
+++ src/components/MediaOverlay/Media/IFrameVideo/IFrameVideo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AssemblyProp from '../../../../prop-types/AssemblyProp';
 
 const IFrameVideo = ({ assembly: { assemblyId } }) => (
-  <iframe title="jwplayer video" src={`/video/ajax/${assemblyId}`} height="100%" width="100%" />
+  <iframe title="jwplayer video" src={`/video/ajax/${assemblyId}`} height="100%" width="100%" scrolling="no" />
 );
 
 IFrameVideo.propTypes = {
